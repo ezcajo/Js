41d9cba8449499751676bfeb5856c7184d674139
elprofemarcelo <elprofemarcelo@hotmail.com>
2019-01-09 22:46:43 -0300

    arreglos parte 1 creacion-funciones

---
+++ Arreglos.js
@@ -0,0 +1,92 @@
+//ARREGLOS
+	//PARTE 1
+	var arreglo = [5, 4, 3, 2, 1];//DECLARARTORIO DE ARREGLOS
+
+	console.log(arreglo);// muestra el arreglo
+	console.log(arreglo[2]); // muestra el valor de la posicion 2
+
+	//FUNCIONES DE ARREGLOS
+		
+		//REVERSE INVIERTE EL ORDEN DEL ARREGLO (REVERSO)
+		arreglo.reverse();
+		console.log(arreglo);
+
+		//MAP Crea un nuevo array con los resultados de la llamada a la función indicada aplicados a cada uno de sus elementos.
+			//SINTAXIS
+				/*var nuevo_array = arr.map(function callback(currentValue, index, array) {
+		     	Elemento devuelto de nuevo_array
+				}[, thisArg])*/
+			//web https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/map
+			var arreglo2 = arreglo.map( function(array){
+				array*=array;// esto funciona como un for en esta parte multiplica el valor de la posicion por si misma(el cuadrado del valor)
+				return array;//envia el valor calculado en el nuevo arreglo;
+			});
+
+			console.log(arreglo2);
+
+			var raices = arreglo2.map(Math.sqrt);
+			console.log(raices);
+
+		//JOIN 
+			/*une todos los elementos de una matriz (o un objeto similar a una matriz)
+			 en una cadena y devuelve esta cadena.*/
+			 //https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/join
+			 //SINTAXIS
+			 	//arr.join([separator])
+
+
+			var arreglo3 =arreglo.join();
+			console.log(arreglo3);
+			console.log(typeof arreglo3);
+			
+			var arreglo3 =arreglo.join('|');
+			console.log(arreglo3);
+
+		//SPLIT CONVIERTE UN STRING EL UN ARREGLO 
+			//USAREMOS EL ARREGLO DEL EJEMPLO ANTERIOR
+			var arreglo4 = arreglo3.split('|');
+			console.log(arreglo4);
+
+		//PUSH AGREGA DATOS AL ARREGLO EN LA ULTIMA POSICION
+		arreglo4.push("11");
+		console.log(arreglo4);		
+
+		//UNSHIFT AGREGA DATOS AL COMIENZO DEL ARREGLO.
+		arreglo4.unshift("22");
+		console.log(arreglo4);		
+
+		//TOSTRING
+			//El método toString() devuelve una cadena de caracteres representando el array especificado y sus elementos.
+				// Este metodo es mas eficiente con referencia la metodo split
+				console.log(arreglo.toString());
+
+		// POP
+			//El método pop() elimina el último elemento de un array y lo devuelve. Este método cambia la longitud del array.
+				//https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/pop
+				console.log(arreglo.pop());
+				console.log(arreglo);
+
+
+		//SPLICE 
+			//El método splice() cambia el contenido de un array eliminando elementos existentes y/o agregando nuevos elementos.
+			//https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/splice
+			//SINTAXIS
+						//array.splice(start[, deleteCount[, item1[, item2[, ...]]]])
+			arreglo.splice(2,2);//PRIMER VALOR REPRESENTA EL INDICE Y EL SEGUNDO VALOR LA CANTIDAD DE ELEMENTOS A ELIMINAR
+			console.log(arreglo);
+
+			arreglo.splice(2,1, 'Marcelo');//PRIMER VALOR REPRESENTA EL INDICE Y EL SEGUNDO VALOR LA CANTIDAD DE ELEMENTOS A ELIMINAR Y EL TERCER ELEMNTO REPRESENTA EL NUEVO VALOR AGREGADO EN LA POSICION DEL ELEMENTO ELIMINADO
+			console.log(arreglo);
+			arreglo.splice(2,0, 'silvia', 'lautaro', 'jony'); //EL SEGUNDO VALOR AHORA NO ELIMINA NINGUN ELEMENTO PERA AGREGAMOS VARIOS ELEMENTOS DESPUES DEL INDICE
+			console.log(arreglo);
+
+		//SLICE
+			//El método slice() devuelve una copia de una parte del array dentro de un nuevo array empezando por inicio hasta fin (fin no incluido). El array original no se modificará.
+			//https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/slice
+
+			//SINTAXIS
+				//arr.slice([inicio [, fin]])
+
+			arreglo = [5, 4, 3, 2, 1];
+			var arreglo5 = arreglo.slice(1, 3);// EL PRIMER NUMERO ES EL INDICE INICIAL Y EL SEGUNO ES EL INDICE FINAL SIN INCLUIR EL VALOR DE ESTE INDICE
+			console.log(arreglo5);
