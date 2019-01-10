4c6a749b3d39ac34174e9ed70370b3e4c32a6c2e
elprofemarcelo <elprofemarcelo@hotmail.com>
2019-01-10 18:39:32 -0300

    Objeto Booleano- caracteristicas

---
+++ ObjetoBoolean.js
@@ -0,0 +1,8 @@
+
+//OBJETOS BOOLEANOS
+	//EL VALOR POR DEFECTO ES FALSE
+	// LOS UNDEFINED, NULL, NAN, STRING VACIO ''TAMBIEN DA COMO VALOR FALSE
+var datos = '';
+var bool = new Boolean(datos);
+
+console.log(bool);
