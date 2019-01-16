ce1e9bdba3bf1e0ad831639b53124b9ff79ebdb8
elprofemarcelo <elprofemarcelo@hotmail.com>
2019-01-16 17:42:19 -0300

    Servidor Web Activacion e Instalacion

---
+++ ServidorWeb.js
@@ -0,0 +1,8 @@
+/*
+1.- Instalar NodeJs para Plataforma windows(https://nodejs.org/es/);
+2.- Instalar un Servidor Web A traves de Node ver Comandos(https://www.npmjs.com/package/http-server):
+	'npm install http-server -g'
+3.-http-server la direccion de la carpeta del proyecto local ejemplo 'C:\Users\LOS PERA\Desktop\JavaScript -p 8081
+4.-http://198.168.66.106:8081
+5.- CTRL +C Detiene el Servidor
+*/
