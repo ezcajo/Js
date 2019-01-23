1ed4d22347a1382832e3e973ef32f3ee8c65ab2a
elprofemarcelo <elprofemarcelo@hotmail.com>
2019-01-23 15:05:23 -0300

    Agregado de Ejemplos archivo Cookies

--- Cookies.js
+++ Cookies.js
@@ -1,4 +1,26 @@
-document.cookie = 'nombre= Marcelo;'
-document.cookie = 'apellido= Peralta;'
+document.cookie = 'nombre=Marcelo';
+document.cookie = 'apllido=Peralta';
 var cookies = document.cookie;
-console.log (cookies);
+console.log(cookies);
+
+function crearCookies(nombre, valor) {
+    valor = escape(valor);
+    var hoy = new Date(); //Creamos un objeto Date
+    hoy.setMonth(hoy.getMonth() + 1); //A la fecha Actual le sumamos un mes
+    return document.cookie = nombre + '=; expires' + hoy.toUTCString() + ';';
+
+}
+
+function eliminarCookies(nombre) {
+    var hoy = new Date(); //Creamos un objeto Date
+    hoy.setMonth(hoy.getMonth() - 1); //A la fecha Actual le restamos  un mes
+    return document.cookie = nombre + '=x ;expires=' + hoy.toUTCString() + ';';
+
+}
+var valorEscape = crearCookies('numero', '1234 ;12, 25 /58');
+console.log(valorEscape);
+
+var valorNoEscapado = unescape(valorEscape);
+console.log(valorNoEscapado);
+
+eliminarCookies('nombre');
