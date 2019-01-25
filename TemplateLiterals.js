a048947d807fd8094f422f574843d3f43c81a4c3
elprofemarcelo <elprofemarcelo@hotmail.com>
2019-01-24 23:57:25 -0300

    Template Lieral Uso Ejemplos

---
+++ TemplateLiterals.js
@@ -0,0 +1,9 @@
+//SIN TEMPLATE LITERALS
+	//
+	let nombre = 'Lautaro';
+	let apellido = 'Peralta';
+
+	console.log(nombre + ' ' + apellido);
+
+// CON TEMPLATE LITERALS
+console.log(`Nombre: ${nombre}\nApellido: ${apellido}`);
