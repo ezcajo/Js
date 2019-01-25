5698d252a2ee659c79d4e653fcc53400b28eaf27
elprofemarcelo <elprofemarcelo@hotmail.com>
2019-01-24 22:07:42 -0300

    Use Strict - ejemplo

---
+++ UsoEstricto.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var nombre = 'Lautaro';
+console.log(nombre);
+
+function getNombre(nombre){
+	"user strict"
+	darNombre = nombre;
+	return darNombre;
+}
+
+console.log(getNombre());
