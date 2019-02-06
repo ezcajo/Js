045035d776ed3a402e46a958f898fb9618390c59
elprofemarcelo <elprofemarcelo@hotmail.com>
2019-02-05 23:27:50 -0300

    Constante Ejemplos

---
+++ constante.js
@@ -0,0 +1,14 @@
+const arreglo = [1, 2, 3];
+//NO SE PUEDE REASIGNAR UNA VARIABLE
+//arreglo = ['Enero', 'Febrero', 'Marzo'];
+console.log(arreglo);
+
+
+// SE PUEDE MODIFICAR LOS DATOS DEL ARRAY
+arreglo[0]='Enero';
+console.log(arreglo);
+
+
+//SE PUEDE AGREGAR MAS ELEMENTOS EN EL ARRAY
+arreglo.push('Banana');
+console.log(arreglo);
