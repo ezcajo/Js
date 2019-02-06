67ea0bf55afa7366ffd0ff8ac090c67631d306e5
elprofemarcelo <elprofemarcelo@hotmail.com>
2019-02-06 20:04:42 -0300

    Actualizacion de Ciclos For

--- CicloFor.js
+++ CicloFor.js
@@ -17,21 +17,38 @@ var alumno = new Persona();
 Persona.prototype.direccion = 'Francia 248';
 
 for (datos in alumno) {
-	//esta funcion nos devuelve un boolean indicando si la propiedad pertenece al objeto permitiendo que el objeto se conozca asi mismo
-	if(alumno.hasOwnProperty(datos)){
+    //esta funcion nos devuelve un boolean indicando si la propiedad pertenece al objeto permitiendo que el objeto se conozca asi mismo
+    if (alumno.hasOwnProperty(datos)) {
 
-		console.log('Estan son propiedades del objeto '+ '\n'+datos + ' : ' + alumno[datos]);
-	} else {
-		
-		console.log('Estan no son propiedades del objeto ' + '\n'+datos + ' : ' + alumno[datos]);
-	}
-	}
+        console.log('Estan son propiedades del objeto ' + '\n' + datos + ' : ' + alumno[datos]);
+    } else {
+
+        console.log('Estan no son propiedades del objeto ' + '\n' + datos + ' : ' + alumno[datos]);
+    }
+}
 //For each
 /* Seutiliza para recoorer un arreglo
+el parametro valor imprimi el valor del elemento recorrido
+el parametro index imprimi el valor del indice del elemento recorrido
 */
 
 var canasto = ['banana', 'Tomate', 'Pera'];
 
-canasto.forEach(function(valor){
-	console.log(valor);
-})
+canasto.forEach(function(valor, index) {
+        console.log(index + ' : ' + valor);
+    })
+    // MAP
+    //recorre un array de Objetos
+let carrito = [
+{id:1, nombre:'Banana'},
+{id:2, nombre: 'Durazno'},
+{id:3, nombre: 'Manzana'},
+{id:4, nombre: 'Pera'}
+];
+
+let impresion = carrito.map(function(carrito){
+	return carrito.nombre;
+
+});
+
+console.log(impresion);
